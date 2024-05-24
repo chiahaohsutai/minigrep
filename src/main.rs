a74e5a0b7e253e514cb7586213376cc14dd26f85
Chia Hao Hsu Tai <hsutai.ch@northeastern.edu>
2024-05-24 19:08:11 -0400

    added parsing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -6,4 +6,9 @@ fn main() {
     let filepath = &args[2];
 
     println!("Searching for \"{query}\" in file: {filepath}.");
+
+    let contents =
+        fs::read_to_string(filepath).expect("Failed to read the file at path: {filepath}");
+
+    println!("With text: {contents}");
 }
