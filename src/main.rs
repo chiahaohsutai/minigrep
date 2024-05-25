c634d3fa94957aefa93423bd9733477eb6a94bc3
Chia Hao Hsu Tai <hsutai.ch@northeastern.edu>
2024-05-25 12:01:47 -0400

    config struct for cmds

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
 use std::{env, fs};
 
+struct Config {
+    query: String,
+    file_path: String,
+}
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Config { query, file_path }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filepath = &args[2];
+    let config = Config::new(&args);
 
-    println!("Searching for \"{query}\" in file: {filepath}.");
+    println!(
+        "Searching for \"{}\" in file: {}.",
+        config.query, config.file_path
+    );
 
     let contents =
-        fs::read_to_string(filepath).expect("Failed to read the file at path: {filepath}");
+        fs::read_to_string(config.file_path).expect("Failed to read the file at path: {filepath}");
 
     println!("With text: {contents}");
 }
